d1bcc7687f7bec7f930f69ad0465d1e224d10ac9
tamar silva <tamar.silva2206@gmail.com>
2022-08-12 12:32:49 -0400

    first commit

---
+++ function myBirthYearFunc(){.js
@@ -0,0 +1,16 @@
+function myBirthYearFunc(){
+    console.log("Nací en " + 1980);
+}
+// el console.log imprime Nací en 1980
+function myBirthYearFunc(EntradaAñoNacimiento){
+        console.log("Nací en " + EntradaAñoNacimiento);
+    }
+//el console.log imprime nací en EntradaAñoNaciomiento
+function add(num1, num2){
+    console.log("¡Sumando números!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+} 
+//el console.log imprime que sum es 30
